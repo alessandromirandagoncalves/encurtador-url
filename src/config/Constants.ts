fba2e3ed7bbc3ab092f692f64975501bcb949e7f
alessandromirandagoncalves <alessandro.inovacao@gmail.com>
2022-07-12 09:50:04 -0300

    Alteração de conexão

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
 	API_URL: 'http://localhost:5000',
 	MONGO_CONNECTION:
-		'mongodb+srv://<user>:<password>@<database>.yptlq.mongodb.net/url-shortener-dio?retryWrites=true&w=majority',
+		'mongodb+srv://teste:teste@mflix.2jixa.mongodb.net/test?authSource=admin&replicaSet=atlas-pbk49t-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
 }
